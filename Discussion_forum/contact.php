4d63e652dc438a4ab4fae279a15b3b1aca2ffbce
Gmunda459 <35957224+Gmunda459@users.noreply.github.com>
2018-02-20 23:33:07 +0530

    Discussion_forum
    PHP Project

---
+++ Discussion_forum/contact.php
@@ -0,0 +1,17 @@
+<?php  require("header.php"); ?>
+<script type="text/javascript">
+	document.getElementById("acontact").className="active";
+</script>
+
+
+	<h1 style ="color:green">Contact Us</h1>
+    <table>
+    
+	
+    <tr><td rowspan="4"><td>Name</td><td>:</td><td>Online Question Answer</td></tr>
+    <tr><td rowspan="1">Contact No</td><td>:</td><td>8537833409</td></tr>
+    <tr><td rowspan="1">E-Mail Add</td><td>:</td><td>gmunda459@gmail.com</td></tr>
+   <tr><td rowspan="1">Website</td><td>:</td><td><a href="http://www.google.com/">www.gogle.com</a></td></tr>
+    </table>
+    
+<?php require("footer.php"); ?>
